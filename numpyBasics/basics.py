a4b1521fa50f1d2e01dbe59d492d53872feda1d8
Mansi Wakharkar <mansi.wakharkar@beyonmoney.com>
2023-01-18 12:02:08 +0300

    Appendig notes and Numpy basics

--- numpyBasics/basics.py
+++ numpyBasics/basics.py
@@ -8,3 +8,9 @@ print("Two dimensional array: ", two_dimenstional_array)
 
 random_integer_array = np.random.randint(low=2, high=10, size=(5))
 print('Array with confined random integer: ', random_integer_array)
+
+arr1 = np.zeros((3,5))
+print('Array of size 3x5: ', arr1)
+
+arr1 = np.zeros((5,3))
+print('Array of size 5x3: ', arr1)
